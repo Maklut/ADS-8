51af3675178c5864be0bd459bae693a0e0276d8d
Egor <149178595+Maklut@users.noreply.github.com>
2026-06-05 20:54:57 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,20 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
-#include <string>
 #include "bst.h"
 
-// Forward declarations
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
 int main() {
-    // Создаем дерево
+    const char* filename = "src/war_peace.txt";
     BST<std::string> tree;
     
-    // Загружаем текст из файла
-    const char* filename = "src/war_peace.txt";
+    std::cout << "Building tree from file: " << filename << std::endl;
     makeTree(tree, filename);
     
-    // Выводим информацию о дереве
     std::cout << "Tree depth: " << tree.depth() << std::endl;
     
-    // Поиск примеров слов
-    std::cout << "Search for 'pierre': " << tree.search("pierre") << " times" << std::endl;
-    std::cout << "Search for 'natasha': " << tree.search("natasha") << " times" << std::endl;
-    std::cout << "Search for 'andrew': " << tree.search("andrew") << " times" << std::endl;
-    
-    // Выводим частотный анализ в файл и на экран
+    std::cout << "\nWord frequencies (sorted by frequency):" << std::endl;
     printFreq(tree);
     
     return 0;
